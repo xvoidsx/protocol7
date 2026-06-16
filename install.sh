6db9ac64ae08bda960d25fcac97df478c15342f3
rav3ndust <35274771+rav3ndust@users.noreply.github.com>
2026-06-16 04:28:06 -0500

    nnn into pkgs, initial bashrc configuration

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
+# early installation script for protocol7
+# XXX more stuff is going to be added soon
 install_pkgs () {
-	local pkgs=("tmux" "vim" "amfora" "lynx" "opendoas")
+	local pkgs=("tmux" "vim" "amfora" "lynx" "opendoas" "nnn")
 	sudo apt update
 	for pkg in $pkgs{@}; do
 		echo "installing $pkg..."; sleep 1
@@ -14,8 +16,10 @@ setup_doas () {
 copy_configs () {
 	local vim_conf="$HOME/.vimrc"
 	local tmux_conf="$HOME/.tmux.conf"
+	local bash_conf="$HOME/.bashrc"
 	doas cp configs/vimrc $vim_conf
 	doas cp configs/tmux.conf $tmux_conf
+	doas cp configs/bashrc $bash_conf
 }
 main () {
 	install_pkgs
