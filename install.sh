c3d3171b954601fea686b04bc8008039765ac502
rav3ndust <35274771+rav3ndust@users.noreply.github.com>
2026-06-16 04:34:08 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env bash
 # early installation script for protocol7
 # XXX more stuff is going to be added soon
+set -euo pipefail
 install_pkgs () {
 	local pkgs=("tmux" "vim" "amfora" "lynx" "opendoas" "nnn")
-	sudo apt update
-	for pkg in $pkgs{@}; do
-		echo "installing $pkg..."; sleep 1
-		sudo apt install $pkg -y
-	done
+	echo "installing needed packages..."; sleep 1
+	sudo apt update; sudo apt install "${pkgs[@]}" -y
 }
 setup_doas () {
 	local doas_conf="/etc/doas.conf"
@@ -17,9 +15,9 @@ copy_configs () {
 	local vim_conf="$HOME/.vimrc"
 	local tmux_conf="$HOME/.tmux.conf"
 	local bash_conf="$HOME/.bashrc"
-	doas cp configs/vimrc $vim_conf
-	doas cp configs/tmux.conf $tmux_conf
-	doas cp configs/bashrc $bash_conf
+	cp configs/vimrc $vim_conf
+	cp configs/tmux.conf $tmux_conf
+	cp configs/bashrc $bash_conf
 }
 main () {
 	install_pkgs
